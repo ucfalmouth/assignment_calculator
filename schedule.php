a680ec7d6d3a50dbd3d4a9a77be1fbebc9311ce6
Tom Readings <digitisatom@gmail.com>
2012-09-26 17:52:53 +0100

    email reminders hidden as incomplete

--- schedule.php
+++ schedule.php
@@ -31,16 +31,19 @@ $(document).ready(function() {
 
 <div id="ach">
 <?php 
-$enddate = $_POST['enddate'];
+
+$enddate = ($_POST['enddate'])? $_POST['enddate'] : date("d-m-y", strtotime(date()." + 1 month"));;
 require("includes/dateDiff.php");
-$today = $_POST['startdate'];
+$today = ($_POST['startdate'])? $_POST['enddate'] : date("d-m-y");;
+
+
 $assignment_end =  dateDiff("-", $enddate, $today);
 
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 // do this in a function?
 require_once("includes/schedule_functions.php");
-require_once('includes/krumo/class.krumo.php');
+//require_once('includes/krumo/class.krumo.php');
 require_once('includes/googledocs.php');
 $googledoc = "https://docs.google.com/spreadsheet/pub?key=0AkXIzcr-VBVDdEMzUDA2Wm5OQ0k4LVRLVFhCTnJ1Vmc&single=true&gid=0&output=csv";
 
@@ -78,11 +81,13 @@ $_SESSION['schedule']=$schedule;
 
         <form action="addEmail.php" method="post"  target="_blank">
   <p>
+    <!--
   <div>If you would like to recieve email reminders of your schedule, please submit your email address:</div>
     <label for="message"></label>
     <input type="text" value="" name="email" placeholder="name@mail.com" class="acinput">
     <input type="submit" value="Please send me email reminders" class="text_button notbut notbutb">
   </p>
   <div class="newbut"><a href="test.php">Create another schedule</a></div>
+    -->
 </form>
 </div>
